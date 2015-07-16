de10a05e23b2d76044c0d96d088dab9111b253b2
KarmaPanda <karmalifeofpanda@gmail.com>
2015-07-16 13:38:44 -0400

    BETA Release

---
+++ AutoFF/Program.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace AutoFF
+{
+    class Program
+    {
+        public static Menu Config;
+
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            Config = new Menu("Auto Surrender", "menu", true);
+
+            Config.AddItem(new MenuItem("toggle", "Auto Surrender at Time Set").SetValue(true));
+            Config.AddItem(new MenuItem("time", "Set Time for Surrender").SetValue(new Slider(20, 15, 120)));
+
+            Notifications.AddNotification("Auto FF Initialized", 10000, true);
+
+            Game.OnUpdate += Game_OnUpdate;
+        }
+
+        static void Game_OnUpdate(EventArgs args)
+        {
+            var time = Config.Item("time").GetValue<Slider>().Value;
+
+            if (Game.ClockTime >= time && Config.Item("toggle").GetValue<bool>())
+            {
+                Game.Say("/ff");
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
